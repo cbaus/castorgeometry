9a9b97c5fea633532099e10c662e3b607c59212e
Colin <colin.baus@kit.edu>
2014-01-23 20:22:07 +0100

    added drawing

--- IR/fit_ir_data.py
+++ IR/fit_ir_data.py
@@ -1,7 +1,8 @@
-import numpy
+import numpy as np
 import math
 import os
-import matplotlib as ml
+import sys
+import matplotlib.pyplot as plt
 from minuit2 import Minuit2 as minuit
 from numpy  import *
 
@@ -20,16 +21,17 @@ def distance_to_beampipe( x , y ):
 
 class castor_half():
     def __init__(self, name,sensor_angles,sensor_pos,rnew):
-        assert len(sensor_angles) == len(rnew), '#angles must be #rnew'
-        assert len(sensor_angles) == len(sensor_pos), '#angles must be #positions'
+        self.nsensors = len(sensor_angles)
+        assert  self.nsensors == len(rnew), '#angles must be #rnew'
+        assert  self.nsensors == len(sensor_pos), '#angles must be #positions'
         if name.find("far") != -1:
-            self.isFarHalf = true
-        else if name.find("near") != -1
-            self.isFarHalf = false
-        else
-            sys.stderr.write("Please choose name with either \"near\" or \"far\" in it")
+            self.isFarHalf = True
+        elif name.find("near") != -1:
+            self.isFarHalf = False
+        else:
+            sys.stderr.write("Please choose name with either \"near\" or \"far\" in it\n")
             exit(1)
-        self.sensor_angles = sensor_angles
+        self.sensor_angles = sensor_angles #x direction is 0. counter-clockwise
         self.sensor_pos = sensor_pos #in ideal geometry
         self.rnew = rnew
         self.verbosity = 1
@@ -39,11 +41,11 @@ class castor_half():
         self.verbosity = verbosity
 
     def fit_pos(self):
-        n = len(self.sensor_angles)
         def f(x,y):
+            #chi2 function. returns distance to beam pipe circle for all vector(sensorpos)-vector(r) for given (x,y) shift
             chi2 = 0
             sigma = 1
-            for i in range(0,n):
+            for i in range(0,self.nsensors):
                 pos = array(self.sensor_pos[i])
                 angle = self.sensor_angles[i]
                 r = self.rnew[i]
@@ -53,10 +55,10 @@ class castor_half():
         m = minuit(f)
         if verbosity > 1 : m.printMode = 1
         m.migrad()
-        x = m.values["x"]
-        y = m.values["y"]
-        chi2 = m.fval / n
-        if verbosity > 0 :print("far half: " if self.far else "near half: ","x=",x," y=",y, "chi2: ", chi2)
+        self.x = m.values["x"]
+        self.y = m.values["y"]
+        chi2 = m.fval / self.nsensors
+        if verbosity > 0 :print "far half: " if self.isFarHalf else "near half: ","x={0:.2f} y={1:.2f} chi2={2:.2f}".format(self.x,self.y,chi2)
         return
 
 verbosity = 1 
@@ -64,17 +66,89 @@ far_angles = [22.5,-67.5]
 far_pos = []
 for angle in far_angles:
     far_pos.append([math.cos(math.radians(angle))*castor_inner_octant_radius , math.sin(math.radians(angle))*castor_inner_octant_radius])
-print("Position of far side sensors is: ", far_pos)
+if verbosity > 0:
+    print "\nPosition of far side sensors is: "
+    for pos in far_pos: print "{0[0]:.3f},{0[1]:.3f}".format(pos)
 
-far_r_old = [10,15]
+far_r_old = [8.68439,20.2883] #top,bottom
+far_r_old_error = [0,0.00508586] #top,bottom
 farside_old = castor_half("farside_old",far_angles,far_pos,far_r_old)
 farside_old.setVerbosity(verbosity)
 farside_old.fit_pos()
 
-far_r_new = [10,20]
+far_r_new = [12.6556,22.2788] #top,bottom
+far_r_new_error = [0,0.00172011] #top,bottom
 farside_new = castor_half("farside_new",far_angles,far_pos,far_r_new)
 farside_new.setVerbosity(verbosity)
 farside_new.fit_pos()
 
-print("Shift due to magnetic field:" 
 
+near_angles = [180-22.5,180+67.5]
+near_pos = []
+for angle in near_angles:
+    near_pos.append([math.cos(math.radians(angle))*castor_inner_octant_radius , math.sin(math.radians(angle))*castor_inner_octant_radius])
+if verbosity > 0:
+    print "\nPosition of near side sensors is: "
+    for pos in near_pos: print "{0[0]:.3f},{0[1]:.3f}".format(pos)
+
+near_r_old = [15.2248,25.8462] #top,bottom
+near_r_old_error = [0.00119269,0.0232433] #top,bottom
+nearside_old = castor_half("nearside_old",near_angles,near_pos,near_r_old)
+nearside_old.setVerbosity(verbosity)
+nearside_old.fit_pos()
+
+near_r_new = [32.9261,32.6869] #top,bottom
+near_r_new_error = [2.44163e-15,0.00781898] #top,bottom
+nearside_new = castor_half("nearside_new",near_angles,near_pos,near_r_new)
+nearside_new.setVerbosity(verbosity)
+nearside_new.fit_pos()
+
+
+
+def printShift(old,new):
+    dx = new.x-old.x
+    dy = new.y-old.y
+    dr = math.sqrt(dx**2+dy**2)
+    print "Shift due to magnetic field (","far  half" if old.isFarHalf else "near half", "): dx={0:.3f} dy={1:.3f} dr={2:.3f}".format(dx,dy,dr)
+
+print("\n\n")
+printShift(farside_old,farside_new)
+printShift(nearside_old,nearside_new)
+
+def draw(old,new):
+
+    circle1=plt.Circle((beampipe_x,beampipe_y),beampipe_r,color='0.8')
+    fig = plt.gcf()
+    fig.gca().add_artist(circle1)
+
+    assert old.nsensors == new.nsensors
+    data_old = []
+    for i in range(0,old.nsensors):
+        data_old.append([old.sensor_pos[i][0]+old.x,old.sensor_pos[i][1]+old.y])
+        
+    if verbosity>1: print "drawing sensors:" << data_old ,"\n", [row[0] for row in data_old], [row[1] for row in data_old]
+    plt.scatter([row[0] for row in data_old], [row[1] for row in data_old], s=5, alpha=0.5, color='r') #s=area
+
+
+    plt.xlabel('x [mm]')
+    plt.ylabel('y [mm]')
+    plt.title('IR Sensor [Jan 07 -> Jan 17]')
+    #plt.text(60, .025, r'$\mu=100,\ \sigma=15$')
+    plt.axis([-50, 50, -50, 50])
+    
+    plt.savefig("test.png")
+
+draw(farside_old,farside_new)
+
+# e = np.e
+# X, Y = np.meshgrid(np.linspace(0, castor_inner_octant_radius, 100), np.linspace(0, castor_inner_octant_radius, 100))
+# F = X ** Y
+# G = Y ** X
+
+# fig = plt.figure()
+# ax = fig.add_subplot(1, 1, 1)
+# plt.axis([-100, 100, -100, 100])
+# plt.contour(X, Y, (F - G), [0])
+# plt.plot([e], [e], 'g.', markersize=20.0)
+# #plt.show()
+# plt.savefig("test.png",bbox_inches=0.100)
